6116d7cbde63a6fc6ca5b42fce913fd390fd14c0
fkaragh <fkfurkankara@gmail.com>
2024-09-27 14:31:00 +0300

    update: signals converted and rjxs added

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,13 +1,24 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class MessagesService {
-    private messages = signal<string[]>([]);
-    allMessages = this.messages.asReadonly();
-    
+    messages$ = new BehaviorSubject<string[]>([]);
+
+    // private messages = signal<string[]>([]);
+    private messages: string[] = [];
+    // allMessages = this.messages.asReadonly();
+    get allMessages(){
+        return [...this.messages];
+    }
+
+    // addMessage(message: string){
+    //     this.messages.update((prevMessages) => [...prevMessages, message]);
+    // }
     addMessage(message: string){
-        this.messages.update((prevMessages) => [...prevMessages, message]);
+        this.messages = [...this.messages, message];
+        this.messages$.next([...this.messages]);
     }
 }
